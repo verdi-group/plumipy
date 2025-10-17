bdbe8b1a28f5c5874cc6411f3741a3b7e6b6d7a5
m-singhal <res.mridul@gmail.com>
2025-10-17 15:40:25 +1000

    Tests: add integration test

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
 
-from plumipy.core import Photoluminescence
+from plumipy.core import Photoluminescence, calculate_spectrum
+from pathlib import Path
 
 
 @pytest.fixture
@@ -18,3 +19,18 @@ def photoluminescence():
 )
 def test_get_direct_mesh(photoluminescence, iv_low, iv_high, result):
     assert all(photoluminescence.get_direct_mesh(iv_low, iv_high, np.pi) == result)
+
+
+def test_calculate_spectrum(data_regression):
+    fixtures_directory = Path(__file__).parent / "fixtures"
+    gs_structure_path = fixtures_directory / "CONTCAR_GS"
+    es_structure_path = fixtures_directory / "CONTCAR_ES"
+    phonons_band_path = fixtures_directory / "band.yaml"
+
+    spectrum_data = calculate_spectrum(
+        gs_structure_path=gs_structure_path,
+        es_structure_path=es_structure_path,
+        phonon_band_path=phonons_band_path,
+    )
+    spectrum_data[0]
+    data_regression.check({"positions_gs": spectrum_data[0].tolist()})
